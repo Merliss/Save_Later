b2ca2a8c77b3a5ffa656a9580633545cca8d5e20
Merlis <merlishjzkl@gmail.com>
2024-03-25 21:26:14 +0100

    Queries for categories lists

---
+++ SaveLater.Application/Functions/Categories/Queries/CategoryInListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveLater.Application.Functions.Categories.Queries
+{
+    public class CategoryInListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+    }
+}
---
+++ SaveLater.Application/Functions/Categories/Queries/GetCategoriesListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SaveLater.Application.Contracts.Persistence;
+using SaveLater.Domain.Entities;
+
+namespace SaveLater.Application.Functions.Categories.Queries
+{
+    public class GetCategoriesListQueryHandler : IRequestHandler<GetCategoriesListQuery, List<CategoryInListViewModel>>
+    {
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoriesListQueryHandler(IMapper mapper, IAsyncRepository<Category> categoryRepository)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<CategoryInListViewModel>> Handle(GetCategoriesListQuery request, CancellationToken cancellationToken)
+        {
+            var all = await _categoryRepository.GetAllAsync();
+            var ordered = all.OrderBy(a => a.Name);
+
+            return _mapper.Map<List<CategoryInListViewModel>>(ordered);
+        }
+
+    }
+}
