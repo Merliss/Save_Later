4c2d45383eb790f88fd8c57442404d57d25ce493
Merlis <merlishjzkl@gmail.com>
2024-03-26 14:25:36 +0100

    Validating for creating posts

---
+++ SaveLater.Application/Functions/Posts/Commands/CreatePost/CreatedPostCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace SaveLater.Application.Functions.Posts.Commands.CreatePost
+{
+    public class CreatedPostCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreatedPostCommandValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+                .NotNull()
+                .MaximumLength(80)
+                .WithMessage("{PropertyName} must not exceed 80 characters");
+
+            RuleFor(p => p.Date)
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+                .NotNull()
+                .LessThan(DateTime.Now.AddDays(1));
+            RuleFor(p => p.Rate)
+                .InclusiveBetween(0, 100)
+                .WithMessage("{PropertyName} must be between 0 and 100");
+
+        }
+
+    }
+}
