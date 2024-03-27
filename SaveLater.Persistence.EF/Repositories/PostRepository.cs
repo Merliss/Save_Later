ba96b3d0578fde99f9a52916c429e1c09d220bc1
Merlis <merlishjzkl@gmail.com>
2024-03-27 14:42:01 +0100

    Post repository for EF created

---
+++ SaveLater.Persistence.EF/Repositories/PostRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaveLater.Application.Contracts.Persistence;
+using SaveLater.Domain.Entities;
+
+namespace SaveLater.Persistence.EF.Repositories
+{
+    public class PostRepository : BaseRepository<Post>, IPostRepository
+    {
+        //private readonly SaveLaterContext _dbContext;
+        public PostRepository(SaveLaterContext dbContext) : base(dbContext) 
+        {
+            
+        }
+        public Task<bool> IsNameAndAuthorAlreadyExist(string title, string author)
+        {
+            var matches = _dbContext.Posts.Any(a => a.Title.Equals(title) && a.Author.Equals(author));
+
+            return Task.FromResult(matches);
+        }
+    }
+}
