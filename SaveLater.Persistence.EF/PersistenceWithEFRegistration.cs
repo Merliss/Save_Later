5ce07763a1db11bd6fec3d710b6f59958e539a72
Merlis <merlishjzkl@gmail.com>
2024-03-27 14:49:32 +0100

    Registration of services for interfaces

---
+++ SaveLater.Persistence.EF/PersistenceWithEFRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SaveLater.Application.Contracts.Persistence;
+using SaveLater.Persistence.EF.Repositories;
+
+namespace SaveLater.Persistence.EF
+{
+    public static class PersistenceWithEFRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SaveLaterContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("SaveLaterConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ICategoryRepository,CategoryRepository>();
+            services.AddScoped<IPostRepository,PostRepository>();
+            services.AddScoped<IEventsRepository,EventRepository>();
+
+            return services;
+
+        }
+    }
+}
