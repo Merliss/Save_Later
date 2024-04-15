1b7b3243a8f9968b253551cceaa729f3e03e157f
Merlis <merlishjzkl@gmail.com>
2024-04-15 19:53:36 +0200

    Swagger added

--- SaveLater.Api/Program.cs
+++ SaveLater.Api/Program.cs
@@ -1,8 +1,21 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using SaveLater.Application;
 using SaveLater.Persistence.EF;
-
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer(); //test
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo
+    {   Version = "v1",
+        Title = "Save Later API",
+
+    });
+   
+});
 
 builder.Services.AddSaveLaterApplication();
 builder.Services.AddSaveLaterPersistenceServices(builder.Configuration);
@@ -16,12 +29,14 @@ builder.Services.AddCors(options =>
 
 });
 
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseCors("Open");
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseEndpoints(endpoints =>
     {
         endpoints.MapControllers();
@@ -31,6 +46,6 @@ app.UseEndpoints(endpoints =>
 
 
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
